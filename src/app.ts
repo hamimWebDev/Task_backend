c8532e0ca3ce40478615a54f46af59f79dbb5f64
hamimDev <mdhamimhowladerasif@gmail.com>
2025-06-22 03:36:04 +0600

    Update more

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,8 @@ const app = express();
 // parser
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173", "https://task-frontend-sigma.vercel.app/"],
+    origin: ["http://localhost:5173", "https://task-frontend-sigma.vercel.app"],
+    //solved all issue
     credentials: true,
 }));
 app.use(cookieParser());
